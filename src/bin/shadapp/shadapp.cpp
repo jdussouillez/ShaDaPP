abeec4c15c6ab4af7a4dc00e60175bd4120a92c9
Junior Dussouillez <junior.dussouillez@gmail.com>
2014-12-10 13:59:08 +0100

    Enable argument parsing

--- src/bin/shadapp/shadapp.cpp
+++ src/bin/shadapp/shadapp.cpp
@@ -68,137 +68,137 @@ int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
 
     // Parse arguments
-//    bool usage = false, version = false;
-//    char* configFile = NULL;
-//    int errorCode = parseArguments(argc, argv, &usage, &version, &configFile);
-//    if (usage) {
-//        printUsage();
-//        return errorCode;
-//    }
-//    if (version) {
-//        printVersion();
-//        return errorCode;
-//    }
-//    if (configFile == NULL) {
-//        std::cerr << "Error: You must specify the configuration file.\n";
-//        std::cerr << "Try '" << APPNAME << " --help' for more information.\n";
-//        return 1;
-//    }
+    bool usage = false, version = false;
+    char* configFile = NULL;
+    int errorCode = parseArguments(argc, argv, &usage, &version, &configFile);
+    if (usage) {
+        printUsage();
+        return errorCode;
+    }
+    if (version) {
+        printVersion();
+        return errorCode;
+    }
+    if (configFile == NULL) {
+        std::cerr << "Error: You must specify the configuration file.\n";
+        std::cerr << "Try '" << APPNAME << " --help' for more information.\n";
+        return 1;
+    }
 
-//    std::cout << "Using file: " << configFile << std::endl;
-    //    shadapp::config::PeerConfig* config = nullptr;
-    //    try {
-    //        config = shadapp::config::ConfigReader::parse(std::string(configFile), "src/resources/shadapp/configSchema.xsd");
-    //        std::cout << "Version = " << config->getVersion()->to_string() << std::endl;
-    //        std::cout << "Id = " << config->getID() << std::endl;
-    //        std::cout << "Port = " << config->getPort() << std::endl;
-    //        std::cout << "Folders :" << std::endl;
-    //        for (auto f : config->getFolders()) {
-    //            std::cout << "\tID = " << f.getId() << std::endl;
-    //            std::cout << "\tPath = " << f.getPath() << std::endl;
-    //            for (auto d : f.getDevices()) {
-    //                std::cout << "\t\tID = " << d->getId() << std::endl;
-    //                std::cout << "\t\tName = " << d->getName() << "  (mem addr: " << d << ")" << std::endl;
-    //                std::cout << std::endl;
-    //            }
-    //        }
-
-    //TODO: "remove this"
-    //        shadapp::protocol::ClusterConfigMessage conf(
-    //                *config->getVersion(),
-    //                config->getName(),
-    //                "blabla",
-    //                config->getFolders(),
-    //                config->getOptions());
-    //        std::vector<uint8_t> bytes = conf.serialize();
-    //        shadapp::protocol::ClusterConfigMessage conf2(bytes);
-    //        std::cout << "OK FINAL" << std::endl;
-    //        std::cout << conf.getVersion() << " - " << conf2.getVersion() << std::endl;
-    //        std::cout << conf.getType() << " - " << conf2.getType() << std::endl;
-    //        std::cout << conf.getClientName() << " - " << conf2.getClientName() << std::endl;
-    //        std::cout << conf.getClientVersion() << " - " << conf2.getClientVersion() << std::endl;
-    //        std::cout << conf.getId() << " - " << conf2.getId() << std::endl;
-    //        std::cout << conf.getFolders().size() << " - " << conf2.getFolders().size() << std::endl;
-    //        for (unsigned int i = 0; i < conf.getFolders().size(); i++) {
-    //            std::cout << std::endl;
-    //            std::cout << conf.getFolders().at(i).getId() << " - " << conf2.getFolders().at(i).getId() << std::endl;
-    //            std::cout << conf.getFolders().at(i).getPath() << " - " << conf2.getFolders().at(i).getPath() << std::endl;
-    //            for (unsigned int j = 0; j < conf.getFolders().at(i).getDevices().size(); j++) {
-    //                std::cout << conf.getFolders().at(i).getDevices().at(j)->getId() << " - " << conf2.getFolders().at(i).getDevices().at(j)->getId() << std::endl;
-    //                std::cout << conf.getFolders().at(i).getDevices().at(j)->getMaxLocalVersion() << " - " << conf2.getFolders().at(i).getDevices().at(j)->getMaxLocalVersion() << std::endl;
-    //                std::cout << std::endl;
-    //            }
-    //        }
-    //        std::cout << std::endl;
-    //        std::cout << conf.getOptions().size() << " - " << conf2.getOptions().size() << std::endl;
-    //        for (auto it : conf.getOptions()) {
-    //            std::cout << it.first << " = " << it.second << std::endl;
-    //        }
-    //        for (auto it : conf2.getOptions()) {
-    //            std::cout << it.first << " = " << it.second << std::endl;
-    //        }
-    //TODO: end "remove this"
-
-
-    // TODO: remove "this"
-    //        std::bitset<4> v;
-    //        v.set(0);
-    //        std::vector<shadapp::fs::BlockInfo> blocks;
-    //        blocks.push_back(shadapp::fs::BlockInfo("my_data_1", 9));
-    //        blocks.push_back(shadapp::fs::BlockInfo("my_data_2", 9));
-    //        std::vector<shadapp::fs::FileInfo> files;
-    //        files.push_back(shadapp::fs::FileInfo("name1", 42, blocks));
-    //        shadapp::protocol::IndexMessage idx1(v, "my_folder", files);
-    //        std::vector<uint8_t> bytes2 = idx1.serialize();
-    //        shadapp::protocol::IndexMessage idx2(bytes2);
-    //        std::cout << "\n\n";
-    //        std::cout << idx1.getType() << " = " << idx2.getType() << std::endl;
-    //        std::cout << idx1.getFolder() << " = " << idx2.getFolder() << std::endl;
-    //        std::cout << idx1.getFiles().size() << " = " << idx2.getFiles().size() << std::endl;
-    //        std::cout << "--> File info : " << std::endl;
-    //        for (unsigned int i2 = 0; i2 < idx1.getFiles().size(); i2++) {
-    //            std::cout << idx1.getFiles().at(i2).getName() << " = " << idx1.getFiles().at(i2).getName() << std::endl;
-    //            std::cout << "-----> Blocks : " << std::endl;
-    //            for (unsigned int i3 = 0; i3 < idx1.getFiles().at(i2).getBlocks().size(); i3++) {
-    //                std::cout << idx1.getFiles().at(i2).getBlocks().at(i3).getSize() << " = " << idx2.getFiles().at(i2).getBlocks().at(i3).getSize() << std::endl;
-    //                std::cout << idx1.getFiles().at(i2).getBlocks().at(i3).getHash() << " = " << idx2.getFiles().at(i2).getBlocks().at(i3).getHash() << std::endl;
-    //            }
-    //        }
-    //        // TODO: end remove "this"
-    //
-    //    } catch (std::exception& e) {
-    //        std::cerr << e.what() << std::endl;
-    //    }
-
-    // TODO: remove this
-    shadapp::Logger::setLevel(shadapp::Logger::Level::ALL);
-    shadapp::Logger::enableColors(true);
-    //    shadapp::Logger::debug("foo");
-    //    shadapp::Logger::info("bar");
-    //    shadapp::Logger::success("Successfully sent !");
-    //    shadapp::Logger::warn("baz");
-    //    std::logic_error ex("err msg");
-    //    shadapp::Logger::error("qux", &ex);
-    //shadapp::Logger::fatal("norf");
-
-    //tests Maxime
-    //shadapp::LocalPeer localPeer(0, std::string(configFile));
-    //shadapp::Network localPeer(0, std::string(configFile));
-    //localPeer.start();
-    //fin test Maxime
-
-    // TODO: remove this test (HASH)
-    std::string hash;
-    uint8_t input[] = {"abc"};
-    shadapp::data::Hash256::hash(input, 3, hash);
-    std::cout << hash << std::endl;
-
+    std::cout << "Using file: " << configFile << std::endl;
+    shadapp::config::PeerConfig* config = nullptr;
     try {
-        app.exec();
+        config = shadapp::config::ConfigReader::parse(std::string(configFile), "src/resources/shadapp/configSchema.xsd");
+        std::cout << "Version = " << config->getVersion()->to_string() << std::endl;
+        std::cout << "Id = " << config->getID() << std::endl;
+        std::cout << "Port = " << config->getPort() << std::endl;
+        std::cout << "Folders :" << std::endl;
+        for (auto f : config->getFolders()) {
+            std::cout << "\tID = " << f.getId() << std::endl;
+            std::cout << "\tPath = " << f.getPath() << std::endl;
+            for (auto d : f.getDevices()) {
+                std::cout << "\t\tID = " << d->getId() << std::endl;
+                std::cout << "\t\tName = " << d->getName() << "  (mem addr: " << d << ")" << std::endl;
+                std::cout << std::endl;
+            }
+        }
+
+        //TODO: "remove this"
+        //        shadapp::protocol::ClusterConfigMessage conf(
+        //                *config->getVersion(),
+        //                config->getName(),
+        //                "blabla",
+        //                config->getFolders(),
+        //                config->getOptions());
+        //        std::vector<uint8_t> bytes = conf.serialize();
+        //        shadapp::protocol::ClusterConfigMessage conf2(bytes);
+        //        std::cout << "OK FINAL" << std::endl;
+        //        std::cout << conf.getVersion() << " - " << conf2.getVersion() << std::endl;
+        //        std::cout << conf.getType() << " - " << conf2.getType() << std::endl;
+        //        std::cout << conf.getClientName() << " - " << conf2.getClientName() << std::endl;
+        //        std::cout << conf.getClientVersion() << " - " << conf2.getClientVersion() << std::endl;
+        //        std::cout << conf.getId() << " - " << conf2.getId() << std::endl;
+        //        std::cout << conf.getFolders().size() << " - " << conf2.getFolders().size() << std::endl;
+        //        for (unsigned int i = 0; i < conf.getFolders().size(); i++) {
+        //            std::cout << std::endl;
+        //            std::cout << conf.getFolders().at(i).getId() << " - " << conf2.getFolders().at(i).getId() << std::endl;
+        //            std::cout << conf.getFolders().at(i).getPath() << " - " << conf2.getFolders().at(i).getPath() << std::endl;
+        //            for (unsigned int j = 0; j < conf.getFolders().at(i).getDevices().size(); j++) {
+        //                std::cout << conf.getFolders().at(i).getDevices().at(j)->getId() << " - " << conf2.getFolders().at(i).getDevices().at(j)->getId() << std::endl;
+        //                std::cout << conf.getFolders().at(i).getDevices().at(j)->getMaxLocalVersion() << " - " << conf2.getFolders().at(i).getDevices().at(j)->getMaxLocalVersion() << std::endl;
+        //                std::cout << std::endl;
+        //            }
+        //        }
+        //        std::cout << std::endl;
+        //        std::cout << conf.getOptions().size() << " - " << conf2.getOptions().size() << std::endl;
+        //        for (auto it : conf.getOptions()) {
+        //            std::cout << it.first << " = " << it.second << std::endl;
+        //        }
+        //        for (auto it : conf2.getOptions()) {
+        //            std::cout << it.first << " = " << it.second << std::endl;
+        //        }
+        //TODO: end "remove this"
+
+
+        // TODO: remove "this"
+        //        std::bitset<4> v;
+        //        v.set(0);
+        //        std::vector<shadapp::fs::BlockInfo> blocks;
+        //        blocks.push_back(shadapp::fs::BlockInfo("my_data_1", 9));
+        //        blocks.push_back(shadapp::fs::BlockInfo("my_data_2", 9));
+        //        std::vector<shadapp::fs::FileInfo> files;
+        //        files.push_back(shadapp::fs::FileInfo("name1", 42, blocks));
+        //        shadapp::protocol::IndexMessage idx1(v, "my_folder", files);
+        //        std::vector<uint8_t> bytes2 = idx1.serialize();
+        //        shadapp::protocol::IndexMessage idx2(bytes2);
+        //        std::cout << "\n\n";
+        //        std::cout << idx1.getType() << " = " << idx2.getType() << std::endl;
+        //        std::cout << idx1.getFolder() << " = " << idx2.getFolder() << std::endl;
+        //        std::cout << idx1.getFiles().size() << " = " << idx2.getFiles().size() << std::endl;
+        //        std::cout << "--> File info : " << std::endl;
+        //        for (unsigned int i2 = 0; i2 < idx1.getFiles().size(); i2++) {
+        //            std::cout << idx1.getFiles().at(i2).getName() << " = " << idx1.getFiles().at(i2).getName() << std::endl;
+        //            std::cout << "-----> Blocks : " << std::endl;
+        //            for (unsigned int i3 = 0; i3 < idx1.getFiles().at(i2).getBlocks().size(); i3++) {
+        //                std::cout << idx1.getFiles().at(i2).getBlocks().at(i3).getSize() << " = " << idx2.getFiles().at(i2).getBlocks().at(i3).getSize() << std::endl;
+        //                std::cout << idx1.getFiles().at(i2).getBlocks().at(i3).getHash() << " = " << idx2.getFiles().at(i2).getBlocks().at(i3).getHash() << std::endl;
+        //            }
+        //        }
+        //        // TODO: end remove "this"
+        //
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
-    //delete config;
-    //app.exit(0);
-    return 0;
-}
+
+        // TODO: remove this
+        shadapp::Logger::setLevel(shadapp::Logger::Level::ALL);
+        shadapp::Logger::enableColors(true);
+        //    shadapp::Logger::debug("foo");
+        //    shadapp::Logger::info("bar");
+        //    shadapp::Logger::success("Successfully sent !");
+        //    shadapp::Logger::warn("baz");
+        //    std::logic_error ex("err msg");
+        //    shadapp::Logger::error("qux", &ex);
+        //shadapp::Logger::fatal("norf");
+
+        //tests Maxime
+        //shadapp::LocalPeer localPeer(0, std::string(configFile));
+        //shadapp::Network localPeer(0, std::string(configFile));
+        //localPeer.start();
+        //fin test Maxime
+
+        // TODO: remove this test (HASH)
+        std::string hash;
+        uint8_t input[] = {"abc"};
+        shadapp::data::Hash256::hash(input, 3, hash);
+        std::cout << hash << std::endl;
+
+        try {
+            app.exec();
+        } catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
+        //delete config;
+        //app.exit(0);
+        return 0;
+    }
